2e1b538f2b67582dd1620d66f9859e7de8b5062a
Mayer <dominik.mayer@sap.com>
2018-09-08 09:56:58 +0200

    Neuer Content

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 window.onscroll = function() {scrollFunction()};
 $(document).ready(function() {
-    adjustheight();
+    for (let ah = 1; ah<=4; ah++){
+        adjustheight(ah);
+    }
   });
 let page1 = "1";
-let page2 = "galerie1";
-let page3 = "ueberuns1";
-let page4 = "kontakt1";
+let page2 = "1";
+let page3 = "1";
+let page4 = "1";
+
+
 
 function scrollFunction(){
     let topnav = document.getElementsByClassName("topnav");
@@ -41,9 +45,9 @@ function wechsleInhalt(seite, id){
     let navneu="";
     let navalt="";
 
-    resetheight();
     switch(seite){
         case "angebote": 
+            resetheight(1);
             elementalt = "angebote" + page1;
             navalt = "leftnava" + page1;
             document.getElementById(elementalt).style.display = "none";
@@ -52,53 +56,70 @@ function wechsleInhalt(seite, id){
             page1 = id;
             if(elementneu == "angebote1"){
                 navneu="leftnava1";
-                document.getElementById("angebotenav").innerHTML = '<img id="bildangebote" src="img/weitere seiten/50 Wegweiser Wanderplan-Seite.jpg" onload="adjustheight();">';
+                document.getElementById("angebotenav").innerHTML = '<img id="bildangebote" src="img/weitere seiten/50 Wegweiser Wanderplan-Seite.jpg" onload="adjustheight(1);">';
             }
             if(elementneu == "angebote2"){
                 navneu="leftnava2";
-                document.getElementById("angebotenav").innerHTML = '<img id="bildangebote2" src="img/weitere seiten/51 Kreuzfelsen vorher Aktionen-Seite.jpg"> <p id="bildangebote2desc">Kreuzfelsen vorher/nachher</p> <img id="bildangebote3" src="img/weitere seiten/52 Kreuzfelsen nachher Aktionen-Seite.jpg" onload="adjustheight();">';
-            }
+                document.getElementById("angebotenav").innerHTML = '<img id="bildangebote2" src="img/weitere seiten/51 Kreuzfelsen vorher Aktionen-Seite.jpg" onload="adjustheight(1);"> <p id="bildangebote2desc">Kreuzfelsen vorher/nachher</p> <img id="bildangebote3" src="img/weitere seiten/52 Kreuzfelsen nachher Aktionen-Seite.jpg" onload="adjustheight(1);">';
+            } 
             if(elementneu == "angebote3"){
                 navneu="leftnava3";
                 let angebote3string = '<div id ="divtourplaner"><a href="https://www.touren-suedschwarzwald.de/de/tour/themenwege/dachsberg-rund-um-das-horbacher-moor-naturlehrpfad-/1522732/" class="divtpchild" target="_blank">Tourenplaner der Ferienwelt Südschwarzwald</a></div>';
                 document.getElementById("angebotenav").innerHTML = angebote3string;
-                adjustheight();
+                adjustheight(1);
                 // -> Hintergrundbild 1. Content document.getElementById("content1").style.backgroundImage = "URL('img/weitere seiten/53 Feldsee Wandertouren-Seite.JPG')";
             }
-            document.getElementById(navalt).classList.remove("selected");
-            document.getElementById(navneu).classList.add("selected");
             break;
         case "galerie": elementalt = page2; 
-            document.getElementById(elementalt).style.visibility = "hidden";
-            document.getElementById(elementneu).style.visibility = "visible";
-            page2 = elementneu;
+            resetheight(2);
+            document.getElementById(elementalt).style.display = "none";
+            document.getElementById(elementneu).style.display = "inline";
+            page2 = id;
             break;
         case "ueberuns": elementalt = page3;
-            document.getElementById(elementalt).style.visibility = "hidden";
-            document.getElementById(elementneu).style.visibility = "visible";
-            page3 = elementneu;
+            resetheight(3);
+            elementalt = "ueberuns" + page3;
+            navalt = "leftnavc" + page3;
+            document.getElementById(elementalt).style.display = "none";
+            document.getElementById(elementneu).style.display = "inline";
+            page3 = id;
+            if(elementneu == "ueberuns1"){
+                navneu="leftnavc1";
+                adjustheight(3);
+            }
+            if(elementneu == "ueberuns2"){
+                navneu="leftnavc2";
+                adjustheight(3);
+            }
             break;
         case "kontakt": elementalt = page4;
-            document.getElementById(elementalt).style.visibility = "hidden";
-            document.getElementById(elementneu).style.visibility = "visible";
-            page4 = elementneu;
+            resetheight(4);
+            document.getElementById(elementalt).style.display = "none";
+            document.getElementById(elementneu).style.display = "inline";
+            page4 = id;
             break;
     }
+    document.getElementById(navalt).classList.remove("selected");
+    document.getElementById(navneu).classList.add("selected");
 }
 
-function resetheight(){
-    document.getElementById("content1").style.height = "auto";
-    document.getElementById("nav1").style.height = "auto";
+function resetheight(id){
+    let content = "content" + id;
+    let nav = "nav" + id;
+    document.getElementById(content).style.height = "auto";
+    document.getElementById(nav).style.height = "auto";
 }
 
-function adjustheight(){
-    let right=document.getElementById("content1").clientHeight;
-    let left=document.getElementById("nav1").clientHeight;
+function adjustheight(id){
+    let content = "content" + id;
+    let nav = "nav" + id;
+    let right=document.getElementById(content).clientHeight;
+    let left=document.getElementById(nav).clientHeight;
     
     if (left>right){
-        document.getElementById("content1").style.height=left;
+        document.getElementById(content).style.height=left;
     }
     else{
-        document.getElementById("nav1").style.height=right;
+        document.getElementById(nav).style.height=right;
     }
 }
